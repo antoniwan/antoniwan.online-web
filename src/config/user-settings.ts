e4eccf892467978b765ce6ba557dbbac7259c3f5
Antonio Rodríguez Martínez <antoniwan@icloud.com>
2026-09-11 08:19:27 -0400

    ++

--- src/config/user-settings.ts
+++ src/config/user-settings.ts
@@ -251,7 +251,7 @@ export const userSettings: UserSettings = {
       title: 'The Bent One',
       url: 'https://the-bent-one-book.stronghandssoftheart.com',
       description:
-        'A bilingual (English and Spanish) kinda-animated web picture book about a short line with a bend, and the shapes a line can take depending on where it stands and who it\'s with',
+        'A bilingual animated web picture book about a short line with a bend, and the shapes a line can take depending on where it stands and who it\'s with',
       category: 'writing',
       icon: 'storybook',
     },
